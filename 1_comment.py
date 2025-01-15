171adc6f6f7e041ed9955583c243b7f184846cde
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-01-15 21:26:03 +0700

    1 comment

---
+++ 1_comment.py
@@ -0,0 +1,20 @@
+# this is the single comment
+
+# these
+# are
+# the
+# multi
+# comment
+
+'''
+These 
+are 
+the 
+multi 
+comment 
+also
+'''
+
+message = "# This is not a comment because it's inside quotes"
+
+print(message)
