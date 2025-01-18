58dac808d036d3accc41eb30b689432f030d0a6c
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-01-19 00:56:01 +0700

    operators in python

---
+++ 4_operators.py
@@ -0,0 +1,69 @@
+# Binding or parenthesized expression, list display, dictionary display, set display
+
+# ( expressions... )
+# [ expressions... ], { key: value... }
+# { expressions }
+
+
+
+# await expression
+# await x
+
+
+# Exponentiation
+# **
+
+
+# Positive, negative, bitwise NOT
+# +x, -x, ~x
+
+
+# Multiplication, matrix multiplication, division, floor division, remainder
+# *, @, /, //, %
+
+
+# Addition and subtraction
+# +, -
+
+
+# Shifts
+# <<, >>
+
+
+# Bitwise AND
+# &
+
+
+# Bitwise XOR
+# ^
+
+
+# Bitwise OR
+# |
+
+# Comparisons, including membership tests and identity tests
+# in, not in, is, is not, <, <=, >, >=, !=, ==
+
+
+# Boolean NOT
+# not x
+
+# Boolean AND
+# and
+
+
+# Boolean OR
+# or
+
+
+# Conditional expression
+# if - else
+
+
+# lambda expression
+# lambda
+
+
+# Assignment Expression
+# :=
+
