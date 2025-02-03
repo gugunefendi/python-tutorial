359275305278f8cd75b9e12aead427e2890626ce
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-02-03 11:06:32 +0700

    Match tuple

--- 11_match.py
+++ 11_match.py
@@ -24,4 +24,22 @@ def check_username(username):
         
 username = check_username("Nada")
 print(username)
-    
+
+# point is an (x, y) tuple
+def match_point(point):
+    match point:
+        case (0, 0):
+            print("Origin")
+        case (0, y):
+            print(f"Y={y}")
+        case (x, 0):
+            print(f"X={x}")
+        case (x, y):
+            print(f"X={x}, Y={y}")
+        case _:
+            raise ValueError("Not a point")
+
+match_point((0, 0))  # Output: Origin
+match_point((0, 5))  # Output: Y=5
+match_point((3, 0))  # Output: X=3
+match_point((3, 4))  # Output: X=3, Y=4
