35334ec29d059ad2578118c47af5b1599a037fcd
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-02-07 13:55:56 +0700

    Aritmethic Expression

---
+++ recap/3_expression.py
@@ -0,0 +1,20 @@
+# Documentation url https://docs.python.org/3/reference/expressions.html
+
+# Expression
+# Expression is a combination from value, variable, operator or function that evaluated to be a single value.
+
+# Arithmetic Conversions
+# Pyhton automatically convert a numeric value to make sure operation runs correctly
+
+# If either argument is a complex number, the other is converted to complex
+x = 3 + 4j + 2 # 
+print(x) # Output: (5+4j)
+
+# if either argument is a floating-point number, the other is converted to float point
+y = 1.2 + 2
+print(y) # Output: 3.2
+
+# if all numbers is intger. no one to convert
+z = 3 + 5
+print(z) # Output: 8
+
